67f4adcf8a65ff0b4573af410e9d17b6d89e96c6
SeaHuyty <seahuyty4444@gmail.com>
2025-07-20 11:34:34 +0700

    Update: morgan for development

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import morgan from 'morgan';
 import automatonRoutes from "./routes/automatonRoutes.js";
 import { initDatabase } from "./database/db.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 // Use routes
 app.use("/api", automatonRoutes);
